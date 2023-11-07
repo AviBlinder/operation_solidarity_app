3d7f738081e74d366f2907fb0b849e4c2e329d89
Avi <aviblinder@gmail.com>
2023-11-07 17:24:47 +0200

    refData provider - fixes

--- frontend_operation_solidarity/constants/index.js
+++ frontend_operation_solidarity/constants/index.js
@@ -1,8 +1,7 @@
 import locations from './israel_cities_names_and__geometric_data.json' assert { type: 'json' };
 import cities_short_list from './cities_short_listl.json' assert { type: 'json' };
-import { statuses, labels } from './generalData';
 
-export { cities_short_list, statuses, labels };
+export { cities_short_list };
 
 export const getLocations = (name) => {
   const location = locations.find(
